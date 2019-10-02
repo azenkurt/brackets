8f9f210fc1014c862edc6564478341f909535a03
freez <sadfreez@gmail.com>
2019-10-02 22:12:37 +0300

    step0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bC) {
+   let strArr = str.split('').sort();
+   let braArr = bracketsConfig.filter( el => (el!='[')&&(el!=']')).sort();
+   return strArr === braArr;
 }
